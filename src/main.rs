ced859bd6adb059d50cbfd3ac5a2491d3e0ed5e0
Zachary Sohovich <zachsohovich@thebriga.de>
2019-11-12 23:32:13 -0800

    added hyper init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate hyper;
+extern crate pretty_env_logger;
+#[macro_use]
+extern crate log;
+
+use hyper::rt::Future;
+use hyper::service::service_fn_ok;
+use hyper::{Body, Response, Server};
+
 fn main() {
-    println!("Hello, world!");
+  pretty_env_logger::init();
+  let addr = ([127, 0, 0, 1], 3000).into();
+
+  let new_service = || service_fn_ok(|_req| Response::new(Body::from("Hello World!")));
+
+  let server = Server::bind(&addr)
+    .serve(new_service)
+    .map_err(|e| eprintln!("server error: {}", e));
+
+  hyper::rt::run(server);
 }
